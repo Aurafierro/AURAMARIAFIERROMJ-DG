5ead909bc01817befd50060766ff122b4527d05a
Aurafierro <auramariafierrofierro@gmail.com>
2024-08-20 20:33:46 -0500

    crud taller1

--- TALLERES/TALLER1/app.js
+++ TALLERES/TALLER1/app.js
@@ -15,7 +15,6 @@ function AgregarTarea() {
    
     alert("Tarea registrada ");
 }
-
 function mostrarTareas() {
     let tareas = JSON.parse(localStorage.getItem("tareas")) || [];
     let cuerpoTabla = document.getElementById("cuerpoTabla");
